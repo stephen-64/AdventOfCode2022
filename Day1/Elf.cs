f6df90a6eb5d476bd1da9fd806123a5364ed5e60
Stephen Pollett <14841823+stephen-64@users.noreply.github.com>
2022-12-01 23:00:47 -0330

    Fixing spacing

--- Day1/Elf.cs
+++ Day1/Elf.cs
@@ -2,7 +2,7 @@ namespace Day1
 {
     class Elf
     {
-        public long calories {get; set;} = 0 ;
+        public long calories {get; set;} = 0;
         public void addCalories(long caloriesCount)
         {
             calories += caloriesCount;
